92a14182c6a72c6b8f1b255e3c581fe6a0be2aed
Mike Caron <caron.mike@gmail.com>
2018-03-18 00:02:31 -0400

    make dev and prod builds

--- webpack.common.js
+++ webpack.common.js
@@ -12,15 +12,6 @@ module.exports = {
 		filename: '[name].[chunkhash].js',
 		path: path.resolve(__dirname, 'dist')
 	},
-	mode: 'development',
-	//mode: 'production',
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './dist',
-		proxy: {
-			"/api": "http://localhost:8000"
-		}
-	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
@@ -55,15 +46,4 @@ module.exports = {
 			}
 		]
 	}
-	/*,optimization: {
-		splitChunks: {
-			cacheGroups: {
-				commons: {
-				    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all"
-				}
-			}
-		}
-	}*/
 };
